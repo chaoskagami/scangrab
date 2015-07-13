69cd454798694149c6ff7bab35b22865b472d4f5
chaoskagami <chaos.kagami@gmail.com>
2015-07-13 04:57:49 -0400

    Automatically commited: regenerate dist

--- dist.sh
+++ dist.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+REPO="$(pwd)"
+
 if [ -d dist ]; then
 	rm -rf dist
 fi
 cd src && BRUTE=1 ./merge && mv dist ../dist
 
 if [ "$1" = "rel" ]; then
+	cd "$REPO"
 	git add .
 	git commit -m "Automatically commited: regenerate dist"
 fi
