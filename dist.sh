7912288be9da825165fafbd46d800614b9b92b52
chaoskagami <chaos.kagami@gmail.com>
2015-07-13 04:55:35 -0400

    Fixed up spinner output

--- dist.sh
+++ dist.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
 if [ -d dist ]; then
 	rm -rf dist
 fi
 cd src && BRUTE=1 ./merge && mv dist ../dist
+
+if [ "$1" = "rel" ]; then
+	git add .
+	git commit -m "Automatically commited: regenerate dist"
+fi
